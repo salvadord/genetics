d91fcc3b8d750ecf9b9985334bcf170ea4c7ee32
salvadord <salvadordura@gmail.com>
2019-07-30 19:17:57 -0400

    made utils.py more generic

--- scripts/utils.py
+++ scripts/utils.py
@@ -89,29 +89,33 @@ def runCommand(command):
     print(stderr)
 
 
-def mpileupAnalysis():
-    inputFile = '../data/BK_SRRsorted_v2.mpileup'
-    quality_thresholds = [20, 25, 30]
-    poly_threshold = 1.0
-    threads = 4
-
+def mpileupAnalysis(inputFile,
+                    positions = [[4008, 5877]],
+                    quality_thresholds = [20, 25, 30],
+                    poly_threshold = 1.0,
+                    threads = 4):
 
     for q in quality_thresholds:
-        # /product="18S ribosomal RNA", positions 4008..5877
-        print('\n\nproduct="18S ribosomal RNA", positions 4008..5877:\n')
-        command = './mpileup-parser-v3.py -f %s -t %s -q %s -s 4008 -e 5877 -p %f' % (inputFile, threads, q, poly_threshold)
-        runCommand(command)
+        for pos in positions:
+            print('\n\nPositions %d..%d with quality threshold %.1f:\n' % (pos[0], pos[1], q))
+            command = './mpileup-parser-v3.py -f %s -t %s -q %s -s %d -e %d -p %f' % (inputFile, threads, q, pos[0], pos[1], poly_threshold)
+            runCommand(command)
 
-        # /product="5.8S ribosomal RNA", positions 6878..7034
-        print('\n\nproduct="5.8S ribosomal RNA", positions 6878..7034:\n')
-        command = './mpileup-parser-v3.py -f %s -t %s -q %s -s 6878 -e 7034 -p %f' % (inputFile, threads, q, poly_threshold)
-        runCommand(command)
+#rDNA - 137410281
 
-        # /product="28S ribosomal RNA", positions 8123..12852
-        print('\n\nproduct="28S ribosomal RNA", positions 8123..12852:\n')
-        command = './mpileup-parser-v3.py -f %s -t %s -q %s -s 8123 -e 12852 -p %f' % (inputFile, threads, q, poly_threshold)
-        runCommand(command)
+# ------------------------------------------------------------------
+# MAIN SCRIPT
+# ------------------------------------------------------------------
 
+# params
+inputFile = '../data/BK_SRRsorted_v2.mpileup'
+positions = [[4008, 5877], # /product="18S ribosomal RNA", positions 4008..5877
+            [6878, 7034],  # /product="5.8S ribosomal RNA", positions 6878..7034
+            [8123, 12852]]  # /product="28S ribosomal RNA", positions 8123..12852
 
+quality_thresholds = [20, 25, 30]
+poly_threshold = 1.0
+threads = 4
 
-mpileupAnalysis()
+# call func
+mpileupAnalysis(inputFile, positions, quality_thresholds, poly_threshold, threads)
